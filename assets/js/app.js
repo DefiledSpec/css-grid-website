f63317c806daf20762d525344b1d932ee6860560
DefiledSpec <defiledspec@gmail.com>
2019-02-21 15:01:28 -0600

    updated footer copyright year

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    const $copyright = $('#copyright')
+    $copyright.html(`&copy; Copyright 2018 - ${new Date().getFullYear()} Jesse Harrah. All Rights Reserved.`)
+})
+
